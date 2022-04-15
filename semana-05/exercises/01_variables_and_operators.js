0221b2cf76ac0ada7253795eacaa1a68a70b534c
SantiagoGonzalez97 <santiagogonzalez97@hotmail.com>
2022-04-15 15:36:40 -0300

    BaSP-2022-Etapa01: Variables and operators

---
+++ semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+/* Variables y operadores:
+Ejercicios: 
+1) a.
+*/
+var numberOne, numberTwo, suma;
+numberOne = 10;
+numberTwo = 15;
+suma = numberOne + numberTwo;
+console.log(suma);
+
+/* 
+1) b.
+*/
+var primerNombre, segundoNombre, nombreCompleto;
+primerNombre = 'Santiago ';
+segundoNombre = 'Gonzalez';
+nombreCompleto = primerNombre + segundoNombre;
+console.log(nombreCompleto);
+
+/*
+1) c.
+*/
+var variableUno, variableDos, variableLength;
+variableUno = 'Radium';
+variableDos = 'Rocket';
+variableLength = variableUno.length + variableDos.length;
+console.log(variableLength);
